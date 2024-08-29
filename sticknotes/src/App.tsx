75f4c79fc3d456951f74e7e487856c380c4c85f8
TadeuN1 <tadeunrs@gmail.com>
2024-08-29 11:48:13 -0300

    ending project

--- sticknotes/src/App.tsx
+++ sticknotes/src/App.tsx
@@ -4,22 +4,57 @@ import './App.css'
 export default function App(){
   const [valores, setValores] = useState("")
   const [tarefas, setTarefas] = useState<string[]>([])
+  const [edicao, setEdicao] = useState({
+    ativo: false,
+    tarefa:''
+  })
 
   function registrador(){
     if(!valores){
+      return
+    }
+    if(edicao.ativo){
+      salvarEdicao();
       return
     }
     setTarefas( tarefas => [...tarefas, valores])
     setValores("")
   }
 
+  function salvarEdicao(){
+    const acharIndexTarefa = tarefas.findIndex( tarefa => tarefa === edicao.tarefa)
+    const todasTarefas = [...tarefas]
 
+    todasTarefas[acharIndexTarefa] = valores;
+    setTarefas(todasTarefas);
+
+    setEdicao({
+      ativo: false,
+      tarefa: ''
+    })
+
+    setValores("")
+
+  }
+
+  function editar(item: string){
+    setValores(item)
+    setEdicao({
+      ativo: true,
+      tarefa: item
+    })
+
+  }
+  function excluir(item: string){
+    const removerTarefa = tarefas.filter(tarefa => tarefa !== item)
+    setTarefas(removerTarefa)
+  }
 
   return(
 
     <div className='container'>
 
-        <h2> Listagem de Tarefas </h2>
+        <h2 className='Title'> Listagem de Tarefas </h2>
 
       <div className='inputInfo'>
         <input placeholder='Digite a tarefa a ser adicionada'
@@ -27,17 +62,22 @@ export default function App(){
         value={valores}
         onChange={(e) => setValores(e.target.value)}
         />
-        <button className='button' onClick={registrador}> Inserir </button>
+        <button className='button' onClick={registrador}> 
+            { edicao.ativo ? "Atualizar Tarefa" : "Adicionar"}
+           </button>
       </div>
 
 
       {tarefas.map((item, index) => (
-        <section key={item}>
+        <section key={item} className='stickers'>
 
-          <div>
+          <div className='info'>
           <span>{item}</span>
-          <button>Editar</button>
-          <button>Excluir</button>
+          </div>
+
+          <div className='actionButtons'>
+          <button onClick={() => editar(item)}>Editar</button>
+          <button onClick={() => excluir(item)}>Excluir</button>
           </div>
 
         </section>
